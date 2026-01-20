48253b0751e55232db6978e9baaf4dd89f202063
Cristiane <mcrisgs09@gmail.com>
2026-01-20 11:08:54 +0100

    Initial commit - ASP.NET Core MVC CRUD application

---
+++ Controllers/ItemController.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CrudMVCApp.Models; // Certifique-se de importar a classe Item
+using System.Collections.Generic;
+
+namespace CrudMVCApp.Controllers
+{
+    public class ItemController : Controller
+    {
+        // Lista de itens em memória
+        private static List<Item> _itens = new List<Item>
+        {
+            new Item { Id = 1, Nome = "Item 1", Descricao = "Descrição do item 1" },
+            new Item { Id = 2, Nome = "Item 2", Descricao = "Descrição do item 2" }
+        };
+
+        public IActionResult Index()
+        {
+            return View(_itens); // Passa a lista de itens para a view
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Item item)
+        {
+            if (ModelState.IsValid)
+            {
+                item.Id = _itens.Count + 1;
+                _itens.Add(item);
+                return RedirectToAction("Index");
+            }
+            return View(item);
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var item = _itens.Find(i => i.Id == id);
+            if (item == null) return NotFound();
+            return View(item);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Item item)
+        {
+            var index = _itens.FindIndex(i => i.Id == item.Id);
+            if (index >= 0)
+            {
+                _itens[index] = item;
+                return RedirectToAction("Index");
+            }
+            return NotFound();
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var item = _itens.Find(i => i.Id == id);
+            if (item == null) return NotFound();
+            return View(item);
+        }
+
+        [HttpPost]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var item = _itens.Find(i => i.Id == id);
+            if (item != null)
+            {
+                _itens.Remove(item);
+                return RedirectToAction("Index");
+            }
+            return NotFound();
+        }
+    }
+}
---
+++ Models/Item.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;  // Necessário para validação
+
+namespace CrudMVCApp.Models
+{
+    public class Item
+    {
+        public int Id { get; set; } // ID único do item
+
+        [Required(ErrorMessage = "O Nome é obrigatório.")] // Adiciona validação para Nome
+        public string Nome { get; set; } // Nome do item
+
+        [Required(ErrorMessage = "A Descrição é obrigatória.")] // Adiciona validação para Descrição
+        public string Descricao { get; set; } // Descrição do item
+    }
+}
+
